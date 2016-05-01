53fca9b299cc3810b64308840c3b220e15abce8e
Martin Müntzing <martin.muntzing@gmail.com>
2016-05-01 21:24:43 +0200

    Trying again to fix .gitignore

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// podio API configuration
-define("CLIENT_ID", "(your-client-id)");
-define("CLIENT_SECRET", "(your-client-secret)");
-define("APP_ID", "(your-app-id)");
-define("APP_TOKEN", "(your-app-token)");
